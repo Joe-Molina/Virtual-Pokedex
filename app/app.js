500b230387b687e55df539fb080fb5c9b196c3fd
Joe Molina <joedodaniljr123@gmail.com>
2023-06-02 00:10:42 -0400

    me traje toda la info necesaria

--- app/app.js
+++ app/app.js
@@ -190,20 +190,22 @@ const d = document,
 
     const infoPokemon = async (pokemon) =>{
 
+        
         let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`),
-            pokemondata = await res.data;
-            infopokemon = {
+        pokemondata = await res.data;
+        console.log(pokemondata.id)
+           let infopokemons = {
+                img: pokemondata.sprites.other.home.front_default,
                 nombre: pokemondata.name,
                 tipo: pokemondata.types[0].type.name,
                 id : pokemondata.id,
                 peso: `${(pokemondata.weight/10)} Kg`,
                 altura: `${pokemondata.height/10} Mts`,
                 region: pokemondata.id,
-                movimientos: { habilidad1: pokemondata.moves[0].move.name,
-                            habilidad2: pokemondata.moves[1].move.name,
-                            habilidad3: pokemondata.moves[2].move.name,
-                            habilidad4: pokemondata.moves[3].move.name,
-                            habilidad5: pokemondata.moves[4].move.name,
+                movimientos: { habilidad1: pokemondata.moves[0].move.name ,
+                            habilidad2: pokemondata.moves[1].move.name ,
+                            habilidad3: pokemondata.moves[2].move.name ,
+                            habilidad4: pokemondata.moves[3].move.name ,
                 },   
                 estadisticas: {
                     hp: pokemondata.stats[0].base_stat,
@@ -215,30 +217,60 @@ const d = document,
                 }            
             }   
                 
-                if(infopokemon.region < 152){
-                    infopokemon.region = "Kanto"
-                } else if(id >= 152 && id <= 251){
-                    infopokemon.region = "Johto"
-                } else if(id >= 252 && id <= 386){
-                    infopokemon.region = "Hoenn"
-                } else if(id >= 387 && id <= 493){
-                    infopokemon.region = "Sinnoh"
-                } else if(id >= 494 && id <= 649){
-                    infopokemon.region = "Teselia"
-                } else if(id >= 650 && id <= 721){
-                    infopokemon.region = "Kalos"
-                } else if(id >= 722 && id <= 809){
-                    infopokemon.region = "Alola"
-                } else if(id >= 810 && id <= 905){
-                    infopokemon.region = "Galar"
-                } else if(id >= 906 && id <= 1010){
-                    infopokemon.region = "Paldea"
+                if(infopokemons.region < 152){
+                    infopokemons.region = "Kanto"
+                } else if(infopokemons.id >= 152 && infopokemons.id <= 251){
+                    infopokemons.region = "Johto"
+                } else if(infopokemons.id >= 252 && infopokemons.id <= 386){
+                    infopokemons.region = "Hoenn"
+                } else if(infopokemons.id >= 387 && infopokemons.id <= 493){
+                    infopokemons.region = "Sinnoh"
+                } else if(infopokemons.id >= 494 && infopokemons.id <= 649){
+                    infopokemons.region = "Teselia"
+                } else if(infopokemons.id >= 650 && infopokemons.id <= 721){
+                    infopokemons.region = "Kalos"
+                } else if(infopokemons.id >= 722 && infopokemons.id <= 809){
+                    infopokemons.region = "Alola"
+                } else if(infopokemons.id >= 810 && infopokemons.id <= 905){
+                    infopokemons.region = "Galar"
+                } else if(infopokemons.id >= 906 && infopokemons.id <= 1010){
+                    infopokemons.region = "Paldea"
                 } 
+            
+            $('.pokemon-info .pokemon-info-img img').src = infopokemons.img;
+            $('.pokemon-info .pokemon-info-nombre').textContent = `Nombre: ${infopokemons.nombre}`;
+            $('.pokemon-info .pokemon-info-tipo').textContent = `Tipo: ${infopokemons.tipo}`;
+            $('.pokemon-info .pokemon-info-id').textContent = `Id: ${infopokemons.id}`;
+            $('.pokemon-info .pokemon-info-peso').textContent = `Peso: ${infopokemons.peso}`;
+            $('.pokemon-info .pokemon-info-altura').textContent = `Altura: ${infopokemons.altura}`;
+            $('.pokemon-info .pokemon-info-region').textContent = `Region: ${infopokemons.region}`;
+            
+            $('.pokemon-info-estadisticas .pokemon-info-hp p').textContent =  `Vida: ${infopokemons.estadisticas.hp}`;
+            $('.pokemon-info-estadisticas .pokemon-info-ataque p').textContent =  `Ataque: ${infopokemons.estadisticas.ataque}`;
+            $('.pokemon-info-estadisticas .pokemon-info-defensa p').textContent =  `Defensa: ${infopokemons.estadisticas.defensa}`;
+            $('.pokemon-info-estadisticas .pokemon-info-atqEsp p').textContent =  `Ataque especial: ${infopokemons.estadisticas.atqEsp}`;
+            $('.pokemon-info-estadisticas .pokemon-info-defEsp p').textContent =  `Defensa especial: ${infopokemons.estadisticas.defEsp}`;
+            $('.pokemon-info-estadisticas .pokemon-info-speed p').textContent =  `Velocidad: ${infopokemons.estadisticas.velocidad}`;
+            
+            while ($('.pokemon-info-habilidades').firstChild) {
+                $('.pokemon-info-habilidades').removeChild($('.pokemon-info-habilidades').firstChild);
+            }        
+            for (let movimiento in infopokemons.movimientos) {
+                $pMovimiento = d.createElement('p')               
+                $pMovimiento.textContent = infopokemons.movimientos[movimiento];               
+                $fragment.appendChild($pMovimiento)
+            }
+            
+            $movimientoTitle = d.createElement('div')
+            $movimientoTitle.textContent = 'movimientos'
+            $movimientoTitle.classList.add('pokemon-info-habilidades-title')
 
-            console.log(infopokemon)
+            $('.pokemon-info-habilidades').appendChild($movimientoTitle)
+            $('.pokemon-info-habilidades').appendChild($fragment)
 
             
-            
+
+        
     }
     
     window.addEventListener('click', async e => {
